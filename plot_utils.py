b287fa7a3577cefb3a748f4f3f7cbd2992b4a024
Stephan Schiffels <stephan.schiffels@mac.com>
2015-05-17 12:40:06 +0200

    added combineCrossCoal method

--- plot_utils.py
+++ plot_utils.py
@@ -1,4 +1,6 @@
 import math
+import bisect
+from scipy.interpolate import interp1d 
 
 class MSMCresult:
     """
@@ -23,12 +25,19 @@ class MSMCresult:
                 l = float(fields[3 + i])
                 self.lambdas[i].append(l)
         self.T = len(self.times_left)
+        self.times_left[0] = self.times_left[1] / 4.0
+        self.times_right[-1] = self.times_right[-2] * 4.0
+    
+    def getInterp(self):
+        x = [0.0] + [0.5 * (tl + tr) for tl, tr in zip(self.times_left, self.times_right)]
+        y = [0.0] + self.lambdas[0]
+        return interp1d(x, y)
 
 def popSizeStepPlot(filename, mu=1.25e-8, gen=30.0):
     """
     to be used with a step-plot function, e.g. matplotlib.pyplot.steps.
     returns (x, y), where x contains the left point of each step-segment in years, and y contains the effective population size. Note that there are two ways to make a step-plot. You should make sure that your step-plot routine moves right and then up/down instead of the other way around.
-    If plotted on a logarithmic x-axis, you should adjust x[0] = x[1] / 4.0, otherwise the leftmost segement will start at 0 and won't be plotted on a log-axis.
+    If plotted on a logarithmic x-axis, you should adjust x[0] = x[1] / 4.0, otherwise the leftmost segment will start at 0 and won't be plotted on a log-axis.
     
     Options:
         mu: Mutation rate per generation per basepair (default=1.25e-8)
@@ -37,6 +46,12 @@ def popSizeStepPlot(filename, mu=1.25e-8, gen=30.0):
     M = MSMCresult(filename)
     x = [t * gen / mu for t in M.times_left]
     y = [(1.0 / l) / (2.0 * mu) for l in M.lambdas[0]]
+    return (x, y)
+
+def coalRatePlot(filename, mu=1.25e-8, gen=30.0):
+    M = MSMCresult(filename)
+    x = [t * gen / mu for t in M.times_left]
+    y = [l for l in M.lambdas[0]]
     return (x, y)
 
 def crossCoalPlot(filename, mu=1.25e-8, gen=30.0):
@@ -48,6 +63,34 @@ def crossCoalPlot(filename, mu=1.25e-8, gen=30.0):
     x = [t * gen / mu for t in M.times_left]
     y = [2.0 * M.lambdas[1][i] / (M.lambdas[0][i] + M.lambdas[2][i]) for i in range(M.T)]
     return (x, y)
+
+def crossCoalPlotCombined(filename1, filename2, filename12, mu=1.25e-8, gen=30.0):
+    """
+    returns (x, y) where x is the time in years and y is the relative cross coalescence rate.
+    This function should be used with msmc2, where the three files correspond to the three different
+    cases within-population1, within-population2 and across populations.
+    Check also the doc-string in popSizeStepPlot for Options and hints on how to plot.
+    """
+    M1 = MSMCresult(filename1)
+    M2 = MSMCresult(filename2)
+    M12 = MSMCresult(filename12)
+    I1 = M1.getInterp()
+    I2 = M2.getInterp()
+    x = []
+    y = []
+    resolution = 10
+    for i in range(M12.T - 1):
+        tLeft = M12.times_left[i]
+        tRight = M12.times_right[i]
+        avgWithinRate = 0.0
+        for j in range(resolution):
+            t = tLeft + j / float(resolution) * (tRight - tLeft)
+            lambda1 = I1(t)
+            lambda2 = I2(t)
+            avgWithinRate += 0.5 * (lambda1 + lambda2) / float(resolution)
+        x.append(tLeft * gen / mu)
+        y.append(M12.lambdas[0][i] / avgWithinRate)
+    return (x, y)     
 
 def tmrcaDistribution(filename, resolution=10, lambda_index=0, mu=1.25e-8, gen=30, cdf=False):
     """
